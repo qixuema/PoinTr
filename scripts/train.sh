10c1abb2f131b2498572971bcaab82b9207fac0f
qixuema <sebastian.xq.ma@gmail.com>
2023-01-14 21:13:41 +0800

    add some comments in *sh file

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# The -x option is one of the options of the set command that is used to enable the debugging feature of the bash script for troubleshooting.
 set -x
-GPUS=$1
+GPUS=$1  # 将命令行的第一个参数赋值给变量 GPUS
 
-PY_ARGS=${@:2}
+PY_ARGS=${@:2} # 将命令行的第二个及后面的参数赋值给变量 PY_ARGS
 
 CUDA_VISIBLE_DEVICES=${GPUS} python main.py ${PY_ARGS}
